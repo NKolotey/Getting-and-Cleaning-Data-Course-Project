177ad274ba86a1e33621463094cd55721cf2225c
Nikolay Kolotey <nkolotey@yahoo.com>
2014-08-18 00:09:22 -0500

    First version

---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+## Load activity labels
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+
+## Load data column names
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+
+## Subset of means and stdevs
+subset_features <- grepl("mean|std", features)
+
+## Load dataset function
+readData <- function(fileSubject, fileX, fileY) {
+	subject <- read.table(fileSubject)
+	X <- read.table(fileX)
+	y <- read.table(fileY)
+
+	names(subject) <- c("subjectID")
+	names(X) <- features
+	names(y) <- c("activityID")
+
+	X <- X[,subset_features]
+
+	cbind(X, y, subject)
+}
+
+## Load test data
+test_data <- readData(
+	"./UCI HAR Dataset/test/subject_test.txt",
+	"./UCI HAR Dataset/test/X_test.txt",
+	"./UCI HAR Dataset/test/y_test.txt"
+	)
+
+## Load train data
+train_data <- readData(
+	"./UCI HAR Dataset/train/subject_train.txt",
+	"./UCI HAR Dataset/train/X_train.txt",
+	"./UCI HAR Dataset/train/y_train.txt"
+	)
+
+## Combine test and train data
+data <- rbind(test_data, train_data)
+
+## Melt data before aggregation
+melt_data <- melt(
+	data,
+	id.vars = c("activityID", "subjectID"),
+	measure.vars = features[subset_features]
+	)
+
+## Aggreagate and find means
+tidy_data <- dcast(
+	melt_data,
+	activityID + subjectID ~ variable,
+	mean)
+
+
+## Substitute activity ID with corresponding Label
+tidy_data$activityID <- activity_labels[tidy_data$activityID]
+names(tidy_data)[1] <- "activityLabel"
+
+## Save the result
+write.table(tidy_data, file = "./tidy_data.txt", row.names = FALSE)
